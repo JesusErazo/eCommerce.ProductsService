60d2348697b5563e68266dc420fef311e04a7640
JesusErazo <erazojesusmateo@gmail.com>
2025-06-22 13:21:20 -0500

    I added products repository

---
+++ DataAccessLayer/Repositories/ProductsRepository.cs
@@ -0,0 +1,68 @@
+﻿using DataAccessLayer.Entities;
+using eCommerce.DataAccessLayer.Context;
+using eCommerce.DataAccessLayer.RepositoryContracts;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace eCommerce.DataAccessLayer.Repositories;
+
+public class ProductsRepository : IProductsRepository
+{
+  private readonly ApplicationDbContext _dbContext;
+
+  public ProductsRepository(ApplicationDbContext dbContext)
+  {
+    _dbContext = dbContext;
+  }
+
+  public async Task<Product?> AddProduct(Product product)
+  {
+    _dbContext.Add(product);
+    await _dbContext.SaveChangesAsync();
+    return product;
+  }
+
+  public async Task<bool> DeleteProduct(Guid ProductID)
+  {
+    bool productExist = await _dbContext.Products.AnyAsync(x => x.ProductID == ProductID);
+
+    if (!productExist)
+    {
+      return false;
+    }
+
+    _dbContext.Remove(new Product { ProductID = ProductID});
+    int affectedRowsCount = await _dbContext.SaveChangesAsync();
+    return affectedRowsCount > 0;
+  }
+
+  public async Task<Product?> GetProductByCondition(Expression<Func<Product, bool>> conditionExpression)
+  {
+    return await _dbContext.Products.FirstOrDefaultAsync(conditionExpression);
+  }
+
+  public async Task<IEnumerable<Product>> GetProducts()
+  {
+    return await _dbContext.Products.ToListAsync();
+  }
+
+  public async Task<IEnumerable<Product?>> GetProductsByCondition(Expression<Func<Product, bool>> conditionExpression)
+  {
+    return await _dbContext.Products.Where(conditionExpression).ToListAsync();
+  }
+
+  public async Task<Product?> UpdateProduct(Product product)
+  {
+    Product? existingProduct = await _dbContext.Products.FirstOrDefaultAsync(x => x.ProductID == product.ProductID);
+
+    if (existingProduct is null) { return null; }
+
+    existingProduct.ProductName = product.ProductName;
+    existingProduct.Category = product.Category;
+    existingProduct.UnitPrice = product.UnitPrice;
+    existingProduct.QuantityInStock = product.QuantityInStock;
+
+    await _dbContext.SaveChangesAsync();
+    return existingProduct;
+  }
+}
